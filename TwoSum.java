fd9260c640af0111feabd76f2c10cf7aa025ae64
Seema MJ <seemamj.94@gmail.com>
2024-05-17 10:52:46 +0530

    Update TwoSum.java

--- TwoSum.java
+++ TwoSum.java
@@ -1,1 +1,15 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+       HashMap<Integer, Integer> complimentMap = new HashMap<>();
+      
+       for( int i = 0; i< nums.length;i++){
+        int compliment = target - nums[i];
+        if(complimentMap.containsKey(compliment)){
+            return new int[]{complimentMap.get(compliment),i};
+        }
+        complimentMap.put(nums[i],i);
+       }
+       return new int[]{};
+}
+}
 
